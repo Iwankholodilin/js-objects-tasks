9c2dac6362e75575af05aecba2f5f9426b1401f2
Iwankholodilin <iwan.kholodilin@yandex.ru>
2025-03-31 21:34:26 +0300

    Add files via upload

---
+++ solutions/4-using.js
@@ -0,0 +1,10 @@
+import { replace } from "lodash";
+
+// BEGIN
+export default (name) => {
+    if (name.startsWith('https')) {
+        return {scheme: 'https', name: name.replace('https://', '')};
+    }
+    return {scheme: 'http', name: name.replace('http://', '')};
+}
+// END
